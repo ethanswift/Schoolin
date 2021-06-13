c85997139a02921b98603ff8a1b0b0876db13d51
ethanswift <ehsonst@gmail.com>
2021-06-13 18:30:46 +0430

    Initial Commit

---
+++ Schoolin'/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schoolin'
+//
+//  Created by ehsan sat on 6/13/21.
+//  Copyright © 2021 ehsan sat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
